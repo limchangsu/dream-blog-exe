f27169bc96e73eb0d191789bd53f38561224ad7e
Mac <mac@Macui-iMac.local>
2023-04-27 00:20:02 +0900

    first2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-    <h1 className='text-3xl text-center'> this is limchangsu blog </h1>
+    <h1 className='text-3xl text-center'> this is limchangsu blog !</h1>
     </div>
   )
 }
